233f7b942e35e9441f632087e1a8018e05e86c4e
Ydv-Suman <sumanydv615@gmail.com>
2025-04-23 19:39:47 -0500

    updated landing page

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       <NavBar />
       <main className="flex-grow px-4 py-8">
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<LandingPage />} />
           <Route path="/mentors" element={<Mentors />} />
           <Route path="/blogs" element={<Blogs />} />
